52064f2ee9599d80f0e374e52b3aa7a4d5912162
Shaunli <1702050003@svuca.edu>
2018-07-02 14:36:01 +0800

    No1
    Signed-off-by: Shaunli <1702050003@svuca.edu>

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const config = require('../config/develop');
-const angentModel = require('../modules/agent').agentModel;
+const userAccountModel = require('../modules/userAccount').userAccountModel;
 const logger = require('../logging/logger');
 const LocalStrategy = require('passport-local').Strategy;
 
@@ -10,14 +10,14 @@ passport.serializeUser(function (user, callback) {
 });
 let foundUser = {};
 passport.deserializeUser(function (username, callback) {
-        angentModel.findOne({'username': username}, function (err, user) {
+    userAccountModel.findOne({'username': username}, function (err, user) {
             callback(err, user);
         });
     }
 );
 passport.use(new LocalStrategy('local', (username, password, callback) => {
         let resultPassowrd = require('crypto').createHash('md5').update(password + config.saltword).digest('hex');
-        angentModel.findOne({'username': username, 'password': resultPassowrd}, (err, data) => {
+    userAccountModel.findOne({'username': username, 'password': resultPassowrd}, (err, data) => {
             foundUser = data;
             if (err) {
                 logger.error('passport: passport.use ' + err);
