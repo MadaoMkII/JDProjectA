26fdbfe5e63202da796e07079a90f1d3254f5718
Shaunli <1702050003@svuca.edu>
2018-07-02 15:10:30 +0800

    No1
    Signed-off-by: Shaunli <1702050003@svuca.edu>

--- main.js
+++ main.js
@@ -4,4 +4,7 @@ http.createServer((req, res) => {
 
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end(JSON.stringify({username:"wahhh"}));
-});
+}).listen(3389);
+
+console.log("Node js is 跑")
+
