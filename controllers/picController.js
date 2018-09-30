3dc22dd2985db6ff2b97d65254c57ad024400320
Shaunli <1702050003@svuca.edu>
2018-09-30 16:29:32 +0800

    修复DA
    Signed-off-by: Shaunli <1702050003@svuca.edu>

--- controllers/picController.js
+++ controllers/picController.js
@@ -57,14 +57,12 @@ exports.uploadImgForEndpoint = async (req, res) => {
     try {
 
         const [returnReq] = await uploadImgAsync(req, res);
-        let imgObject = [];
+
         logger.info(returnReq.file);
         // if (tool.isEmpty(returnReq.file)) {
         //     return res.status(400).json({error_msg: `图片获取为空`, error_code: "400"});
         // }
-        imgObject.push(`http://www.yubaopay.com.tw/image/${returnReq.file.filename}`);
-
-        return res.json({error_msg: `OK`, error_code: "0", data: imgObject});
+        return res.json({error_msg: `OK`, error_code: "0", data: `http://www.yubaopay.com.tw/image/${returnReq.file.filename}`});
 
     }
     catch (e) {
