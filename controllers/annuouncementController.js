632e7be946266dead6e0ce5fafaccd67650b3d39
Shaunli <1702050003@svuca.edu>
2018-10-30 00:35:26 +0800

    Revert "wechat 0.11"
    This reverts commit 25f9277
    
    Signed-off-by: Shaunli <1702050003@svuca.edu>

--- controllers/annuouncementController.js
+++ controllers/annuouncementController.js
@@ -29,7 +29,7 @@ exports.findAnnouncement = async (req, res) => {
         _id: 0
     }, operator, async (err, data) => {
         if (err) {
-            return res.json({error_msg: `400`, error_code: "advertising Error"});
+            return res.status(500).json({error_msg: `500`, error_code: "advertising Error"});
         } else {
             let billCount = await announcementModel.count(searchCommand);
             return res.json({error_msg: `OK`, error_code: "0", data: data, nofdata: billCount});
@@ -44,7 +44,7 @@ exports.updateHelpCenterAnnouncement = async (req, res) => {
         if (!isEmpty(req.body.model_name)) {
             let anResult = await anModel.findOne({name: req.body.model_name});
             if (isEmpty(anResult)) {
-                return res.status(400).json({error_msg: `400`, error_code: "model_name is empty"});
+                return res.status(406).json({error_msg: `406`, error_code: "model_name is empty"});
             } else {
                 searchCommand.model = anResult._id;
 
@@ -60,7 +60,7 @@ exports.updateHelpCenterAnnouncement = async (req, res) => {
             searchCommand.announcementTopic = req.body.announcementTopic;
         }
         if (isEmpty(req.body.announcementID)) {
-            return res.status(400).json({error_msg: `400`, error_code: "announcementID can not be empty"});
+            return res.status(406).json({error_msg: `406`, error_code: "announcementID can not be empty"});
         }
 
         let returnResult = await announcementModel.findOneAndUpdate({announcementID: req.body.announcementID}, {
@@ -69,8 +69,8 @@ exports.updateHelpCenterAnnouncement = async (req, res) => {
 
         return res.json({error_msg: `OK`, error_code: "0", data: returnResult});
     } catch (err) {
-        console.log(err)
-        return res.status(400).json({error_msg: `400`, error_code: "announcement Error"});
+
+        return res.status(500).json({error_msg: `500`, error_code: "announcement Error"});
     }
 
 };
@@ -93,7 +93,7 @@ exports.updateAnnouncement = async (req, res) => {
             searchCommand.announcementTopic = req.body.announcementTopic;
         }
         if (isEmpty(req.body.announcementID)) {
-            return res.status(400).json({error_msg: `400`, error_code: "announcementID can not be empty"});
+            return res.status(406).json({error_msg: `406`, error_code: "announcementID can not be empty"});
         }
 
         let returnResult = await announcementModel.findOneAndUpdate({announcementID: req.body.announcementID}, {
@@ -102,15 +102,15 @@ exports.updateAnnouncement = async (req, res) => {
 
         return res.json({error_msg: `OK`, error_code: "0", data: returnResult});
     } catch (err) {
-        console.log(err)
-        return res.status(400).json({error_msg: `400`, error_code: "announcement Error"});
+
+        return res.status(500).json({error_msg: `500`, error_code: "announcement Error"});
     }
 
 };
 exports.addModel = async (req, res) => {
     try {
         if (isEmpty(req.body.model_name)) {
-            return res.status(400).json({error_msg: `400`, error_code: "model_name can not be empty"});
+            return res.status(406).json({error_msg: `406`, error_code: "model_name can not be empty"});
         }
         let anModelEntity = new anModel();
         anModelEntity.name = req.body.model_name;
@@ -121,11 +121,26 @@ exports.addModel = async (req, res) => {
 
         if (err.message.toString().search(`duplicate key error`) !== 0) {
 
-            return res.status(400).json({error_msg: `400`, error_code: "model_name can not be duplicated"});
+            return res.status(409).json({error_msg: `409`, error_code: "model_name can not be duplicated"});
         }
-        return res.status(400).json({error_msg: `400`, error_code: "model add Error"});
+        return res.status(500).json({error_msg: `500`, error_code: "model add Error"});
     }
 
+};
+exports.updateModel = async (req, res) => {
+
+    try {
+        if (isEmpty(req.body.model_name) || isEmpty(req.body[`new_model_name`])) {
+            return res.status(406).json({
+                error_msg: `406`,
+                error_code: "model_name or new_model_name can not be empty"
+            });
+        }
+        await anModel.findOneAndUpdate(req.body.model_name, {$set: {name: req.body[`new_model_name`]}});
+        return res.json({error_msg: `OK`, error_code: "0"});
+    } catch (err) {
+        return res.status(500).json({error_msg: `500`, error_code: "model add Error"});
+    }
 };
 
 exports.getModel = async (req, res) => {
@@ -134,7 +149,7 @@ exports.getModel = async (req, res) => {
         let result = await anModel.find();
         return res.json({error_msg: `OK`, error_code: "0", data: result});
     } catch (err) {
-        return res.status(400).json({error_msg: `400`, error_code: "model add Error"});
+        return res.status(500).json({error_msg: `500`, error_code: "model add Error"});
     }
 
 };
@@ -143,13 +158,13 @@ exports.removeModel = async (req, res) => {
     try {
 
         if (isEmpty(req.body.model_name)) {
-            return res.status(400).json({error_msg: `400`, error_code: "model_name can not be empty"});
+            return res.status(406).json({error_msg: `406`, error_code: "model_name can not be empty"});
         }
         await anModel.remove({name: req.body.model_name});
 
         return res.json({error_msg: `OK`, error_code: "0"});
     } catch (err) {
-        return res.status(400).json({error_msg: `400`, error_code: "model add Error"});
+        return res.status(500).json({error_msg: `500`, error_code: "model add Error"});
     }
 
 };
@@ -158,14 +173,14 @@ exports.removeModel = async (req, res) => {
 exports.getHelpCenterAnnouncement = async (req, res) => {
 
     let result = await announcementModel.find({location: 'helpCenter'}).populate(`model`);
-    return res.json({error_msg: `OK`, error_code: "0", data: result});
+    return res.status(200).json({error_msg: `OK`, error_code: "0", data: result});
 };
 
 
 exports.addHelpCenterAnnouncement = async (req, res) => {
 
     if (isEmpty(req.body.model_name)) {
-        return res.status(400).json({error_msg: `400`, error_code: "model_name can not be empty"});
+        return res.status(406).json({error_msg: `406`, error_code: "model_name can not be empty"});
     }
     let anModelEntity;
     let anEntity = await anModel.findOne({name: req.body.model_name});
@@ -179,7 +194,6 @@ exports.addHelpCenterAnnouncement = async (req, res) => {
 
 
     announcementObject.model = anModelEntity._id;
-    console.log(announcementObject.model)
     announcementObject.content = req.body.content;
     announcementObject.link = req.body.link.trim();
     announcementObject.announcementTopic = req.body.announcementTopic;
@@ -187,8 +201,7 @@ exports.addHelpCenterAnnouncement = async (req, res) => {
 
     announcementObject.save(err => {
         if (err) {
-            console.log(err)
-            return res.status(400).json({error_msg: `400`, error_code: "announcement Error"});
+            return res.status(500).json({error_msg: `500`, error_code: "announcement Error"});
         } else {
             return res.json({error_msg: `OK`, error_code: "0"});
         }
@@ -208,7 +221,7 @@ exports.addAnnouncement = (req, res) => {
     announcementObject.save(err => {
         if (err) {
 
-            return res.status(400).json({error_msg: `400`, error_code: "announcement Error"});
+            return res.status(500).json({error_msg: `500`, error_code: "announcement Error"});
         } else {
             return res.json({error_msg: `OK`, error_code: "0"});
         }
@@ -221,9 +234,9 @@ exports.delAnnouncement = (req, res) => {
 
     announcementModel.remove({announcementID: item_id}, (err) => {
         if (err) {
-            return res.json({error_msg: `400`, error_code: "advertising Error"});
+            return res.status(500).json({error_msg: `500`, error_code: "advertising Error"});
         } else {
-            return res.json({error_msg: `OK`, error_code: "0"});
+            return res.status(200).json({error_msg: `OK`, error_code: "0"});
         }
     })
 };
