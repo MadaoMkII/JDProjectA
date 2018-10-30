54b9a5d2f5b385657050c6bc10a6cd6f20ca54d0
Administrator <gogosdsd@gmail.com>
2018-10-30 11:49:17 +0800

    announcementLink

--- router.js
+++ router.js
@@ -62,21 +62,22 @@ app.use((req, res, next) => {
     //buyao res.setHeader('Access-Control-Allow-Origin', `*`);
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     //res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader("Content-Type",'application/json');
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        // Pass to next layer of middleware
-        next();
-    }
+    // res.setHeader("Content-Type",'application/json');
+    // if (req.method === 'OPTIONS') {
+    //     res.sendStatus(200);
+    // } else {
+    //     // Pass to next layer of middleware
+    //     next();
+    // }
+    next();
 });
 // In middleware
 app.use(function (req, res, next) {
