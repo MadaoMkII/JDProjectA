31180b4866a8277e068cb0c1fef1252daecc6d47
Shaunli <1702050003@svuca.edu>
2018-11-16 10:01:39 +0800

    Merge branch 'develop' of https://github.com/MadaoMkII/JDProjectA into develop
    Signed-off-by: Shaunli <1702050003@svuca.edu>
    
    # Conflicts:
    #	.gitignore

--- ceshi.js
+++ ceshi.js
@@ -1,27 +1,27 @@
-// let TopClient = require('topsdk');
-// var client = new TopClient({
-//     'appkey': '25260079',
-//     'appsecret': '70b1bcc4ff485e28a44d5c33e8b3b01c',
-//     'REST_URL': 'http://gw.api.taobao.com/router/rest'
-// });
-//
-// client.execute('taobao.tbk.item.get', {
-//     'fields':'num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url,seller_id,volume,nick',
-//     'q':'女装',
-//     'cat':'16,18',
-//     'itemloc':'杭州',
-//     'sort':'tk_rate_des',
-//     'is_tmall':'false',
-//     'is_overseas':'false',
-//     'start_price':'10',
-//     'end_price':'10',
-//     'start_tk_rate':'123',
-//     'end_tk_rate':'123',
-//     'platform':'1',
-//     'page_no':'123',
-//     'page_size':'20'
-// }, function(error, response) {
-//     if (!error) console.log(response);
-//     else console.log(error);
-// })
-console.log(`Account2:` + ("eb23faf0-db3a-11e8-8fba-75e292622d53|asdasdd").split(`|`)[1]);
+let TopClient = require('./sdk-nodejs').ApiClient;
+var client = new TopClient({
+    'appkey': '25260079',
+    'appsecret': '70b1bcc4ff485e28a44d5c33e8b3b01c',
+    'REST_URL': 'http://gw.api.taobao.com/router/rest'
+});
+client.execute('taobao.tbk.uatm.favorites.get', {
+    'page_no':'1',
+    'page_size':'20',
+    'fields':'favorites_title,favorites_id,type',
+    'type':'1'
+}, function(error, response) {
+    if (!error) console.log(response.results.tbk_favorites);
+    else console.log(error);
+})
+client.execute('taobao.tbk.uatm.favorites.item.get', {
+    'platform':'1',
+    'page_size':'20',
+    'adzone_id':'57411200488',
+    'unid':'3456',
+    'favorites_id':'18853340',
+    'page_no':'1',
+    'fields':'num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url,seller_id,volume,nick,shop_title,zk_final_price_wap,event_start_time,event_end_time,tk_rate,status,type'
+}, function(error, response) {
+    if (!error) console.log(response.results);
+    else console.log(error);
+})
