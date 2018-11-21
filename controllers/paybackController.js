8e147d0aa7528593a1100c1a2e3f5745e355f053
Shaunli <1702050003@svuca.edu>
2018-11-21 09:50:22 +0800

    Merge branch 'develop' of https://github.com/MadaoMkII/JDProjectA into develop
    Signed-off-by: Shaunli <1702050003@svuca.edu>
    
    # Conflicts:
    #	.gitignore

--- controllers/paybackController.js
+++ controllers/paybackController.js
@@ -17,7 +17,7 @@ let getItemInfo = (urlLink) => {
 
         client.execute('taobao.tbk.item.info.get', {
             'num_iids': itemID,
-            'outer_code':`GUNDAM`,
+            'outer_code': `GUNDAM`,
             'platform': '1',
             'ip': '11.22.33.43'
         }, (err, data) => {
@@ -38,3 +38,76 @@ let xRunner = (async () => {
     let ob = await getItemInfo('https://item.taobao.com/item.htm?spm=a219t.7900221/10.1998910419.d5d3d3cdd.685475a5ZqdaZl&id=558479490380');
     console.log(ob[0]);
 })();
+exports.getThisUserPayback = async (req, res) => {
+    let resultArray2 = [
+        {
+            taobaoOrderID: `00296871994`,
+            email_address: `nihao@126.com`,
+            orderStatus: `running`,
+            paybackAmount: 300,
+            paybackStatus: 1,
+            RcoinReturn: 12,
+            dealDate: `2018-09-60`
+        },
+        {
+            taobaoOrderID: `00296871994`,
+            email_address: `nihao@126.com`,
+            orderStatus: `running`,
+            paybackAmount: 300,
+            paybackStatus: 1,
+            RcoinReturn: 12,
+            dealDate: `2018-09-60`
+        },
+        {
+            taobaoOrderID: `00296871994`,
+            email_address: `nihao@126.com`,
+            orderStatus: `running`,
+            paybackAmount: 300,
+            paybackStatus: 1,
+            RcoinReturn: 12,
+            dealDate: `2018-09-60`
+        },
+        {
+            taobaoOrderID: `00296871994`,
+            email_address: `nihao@126.com`,
+            orderStatus: `running`,
+            paybackAmount: 300,
+            paybackStatus: 1,
+            RcoinReturn: 12,
+            dealDate: `2018-09-60`
+        },
+        {
+            taobaoOrderID: `00296871994`,
+            email_address: `nihao@126.com`,
+            orderStatus: `running`,
+            paybackAmount: 300,
+            paybackStatus: 1,
+            RcoinReturn: 12,
+            dealDate: `2018-09-60`
+        },
+        {
+            taobaoOrderID: `00296871994`,
+            email_address: `nihao@126.com`,
+            orderStatus: `running`,
+            paybackAmount: 300,
+            paybackStatus: 1,
+            RcoinReturn: 12,
+            dealDate: `2018-09-60`
+        },
+        {
+            taobaoOrderID: `00296871994`,
+            email_address: `nihao@126.com`,
+            orderStatus: `running`,
+            paybackAmount: 300,
+            paybackStatus: 1,
+            RcoinReturn: 12,
+            dealDate: `2018-09-60`
+        }
+
+    ];
+
+    return res.status(200).send({
+        error_code: 0, error_msg: "OK", data: resultArray2, nofdata: count
+    });
+
+};
