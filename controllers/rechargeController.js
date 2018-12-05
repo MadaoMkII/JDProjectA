12f0c2ccd62b4f678e0305b8b364b437919d04aa
Administrator <1702050003@svuca.edu>
2018-12-06 01:56:52 +0800

    Buffer from

--- controllers/rechargeController.js
+++ controllers/rechargeController.js
@@ -15,7 +15,7 @@ let getUserInfo = (req) => {
     userObject.email_address = req.user.email_address;
     userObject.realName = tool.isEmpty(req.user.realName) ? `尚未实名` : req.user.realName;
     userObject.nickName = req.user.nickName;
-    userObject.Rcoins = req.user.Rcoins;
+    userObject.Rcoins = tool.decrypt(req.user.Rcoins);
     userObject.VIPLevel = req.user.VIPLevel;
     return userObject;
 };
