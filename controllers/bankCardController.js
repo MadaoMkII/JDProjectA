eef7eedd14f45713da74cee80ed5833ca678ba5c
Administrator <1702050003@svuca.edu>
2018-12-07 14:41:49 +0800

    Revert "Buffer from"
    This reverts commit b2364ac

--- controllers/bankCardController.js
+++ controllers/bankCardController.js
@@ -69,7 +69,7 @@ exports.getCardRequest = async (req, res) => {
                         "cur": "NTD",
                         "order_desc": "Testing",
                         "capt_flag": "1",
-                        "order_no": "NO012345678",
+                        "order_no": "NO0123905678",
                         "result_flag": "0",
                         "post_back_url": "http://www.yubaopay.com.tw/cardReceive",
                         "result_url": "https://www.baidu.com",
