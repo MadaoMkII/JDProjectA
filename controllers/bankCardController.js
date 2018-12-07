4d050ecb85d2ec7310131f1a18b42987e3d5b0fa
Administrator <1702050003@svuca.edu>
2018-12-07 17:58:13 +0800

    Revert "Buffer from"
    This reverts commit b2364ac

--- controllers/bankCardController.js
+++ controllers/bankCardController.js
@@ -29,7 +29,7 @@ exports.returnMoney = async (req, res) => {
         "mid":"999812666555013",
         "tid":"T0000000",
         "pay_type":1,
-        "tx_type":req.query.type,
+        "tx_type":8,
         "params":
             {
                 "order_no":req.query.orderID
