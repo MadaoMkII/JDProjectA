1fc946df293576fdc2532528ec2bb325536ccec5
Administrator <1702050003@svuca.edu>
2018-12-07 14:40:27 +0800

    Revert "Buffer from"
    This reverts commit b2364ac

--- controllers/bankCardController.js
+++ controllers/bankCardController.js
@@ -22,11 +22,38 @@ exports.receiveCardRequest = async (req, res) => {
     res.status(200).json({requestQuery: req.query, requestNody: req.body});
 
 };
+exports.sendMoney = async (req, res) => {
+    let requestBody_1 =
+        {
+            "sender": "rest",
+            "ver": "1.0.0",
+            "mid": "999812666555013",
+            "tid": "T0000000",
+            "pay_type": 1,
+            "tx_type": 1,
+            "params":
+                {
+                    "amt": "2000",
+                    "layout": "1",
+                    "cur": "NTD",
+                    "order_desc": "Testing",
+                    "capt_flag": "0",
+                    "order_no": "NO012345678",
+                    "result_flag": "1",
+                    "post_back_url": "http://www.yubaopay.com.tw/cardReceive",
+                    "result_url": "https://www.baidu.com",
+                    "pan":"4162050100018705",
+                    "exp_date":"3509",
+                    "cvv2":"CVV2",
 
+                }
+        };
+    let [, result_1] = await requestFun(requestBody_1, "POST", "https://tspg-t.taishinbank.com.tw/tspgapi/restapi/auth.ashx");
+
+}
 
 exports.getCardRequest = async (req, res) => {
     try {
-
         let requestBody_1 =
             {
                 "sender": "rest",
@@ -41,37 +68,37 @@ exports.getCardRequest = async (req, res) => {
                         "layout": "1",
                         "cur": "NTD",
                         "order_desc": "Testing",
-                        "capt_flag": "0",
+                        "capt_flag": "1",
                         "order_no": "NO012345678",
-                        "result_flag": "1",
+                        "result_flag": "0",
                         "post_back_url": "http://www.yubaopay.com.tw/cardReceive",
                         "result_url": "https://www.baidu.com",
-                        "pan":"4162050100018705",
-                        "exp_date":"3509",
-                        "cvv2":"CVV2",
+                        // "pan":"4162050100018705",
+                        // "exp_date":"3509",
+                        // "cvv2":"CVV2",
 
                     }
             };
         let [, result_1] = await requestFun(requestBody_1, "POST", "https://tspg-t.taishinbank.com.tw/tspgapi/restapi/auth.ashx");
         console.log(result_1)
 
-        let requestBody =
-            {
-                "sender": "rest",
-                "ver": "1.0.0",
-                "mid": "999812666555013",
-                "tid": "T0000000",
-                "pay_type": 1,
-                "tx_type": 3,
-                "params":
-                    {
-                        "amt": "18900",
-                        "order_no": "NO012345678"
-                    }
-            };
-
-        let [, result] = await requestFun(requestBody, "POST", "https://tspg-t.taishinbank.com.tw/tspgapi/restapi/other.ashx");
-        console.log(result)
+        // let requestBody =
+        //     {
+        //         "sender": "rest",
+        //         "ver": "1.0.0",
+        //         "mid": "999812666555013",
+        //         "tid": "T0000000",
+        //         "pay_type": 1,
+        //         "tx_type": 3,
+        //         "params":
+        //             {
+        //                 "amt": "18900",
+        //                 "order_no": "NO012345678"
+        //             }
+        //     };
+        //
+        // let [, result] = await requestFun(requestBody, "POST", "https://tspg-t.taishinbank.com.tw/tspgapi/restapi/other.ashx");
+        // console.log(result)
         // let [, result2] = await requestFun({}, "get", result.params[`hpp_url`]);
         console.log(`__________________________________________________________`);
 
@@ -79,7 +106,7 @@ exports.getCardRequest = async (req, res) => {
         // res.write(result2);
         // res.end();
         //res.redirect(301, result.params[`hpp_url`]);
-        res.status(200).json({data: result});
+        res.status(200).json({data: result_1});
     } catch (err) {
         console.log(err)
         return res.status(503).json({error_msg: `503`, error_code: err.message});
