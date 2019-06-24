0538ea56bd7a33e6786e4acddff48d664cc5170c
Shaunli <1702050003@svuca.edu>
2019-06-24 18:53:47 +0800

    wechat
    Signed-off-by: Shaunli <1702050003@svuca.edu>

--- ceshi.js
+++ ceshi.js
@@ -1,11 +1,11 @@
 const tool = require('./config/tools');
 //
-// console.log(tool.decrypt("632f0be607655a592bbb51483108b81d$e4288aaa6f5d95aeadd10f974ce97ddb$85c1db1f68fee7d69e9e2ed865c6d173cf6eef797195567355643e4183fbcad4"));
-console.log(tool.encrypt(5500.58))
+console.log(tool.decrypt("b5e5d26365dd167496003bb95aeebddc$dbb5dae180810c473f0ba82465236495$cc692bb4981ed8b72921a73491187ef03bdc120c58d3f7c2e536e47344608f90"));
+//console.log(tool.encrypt(5500.58))
 //console.log(Number("29.6")+0.4)
 var str = "CHARAL10701000534821";
 console.log(/^(CHAR)/.test(str));
-
+console.log(320+420+372+238+105);
 // const dgBillModel = require('./modules/dgBill').dgBillModel;
 //
 //
