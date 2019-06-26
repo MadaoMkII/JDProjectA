38f805ea9d0f102f358f390f9699a4cf1f6edcda
Shaunli <1702050003@svuca.edu>
2019-06-26 18:03:25 +0800

    wechat
    Signed-off-by: Shaunli <1702050003@svuca.edu>

--- modules/userAccount.js
+++ modules/userAccount.js
@@ -228,7 +228,7 @@ userAccountSchema.set('toJSON', {
         delete ret._id;
         delete ret.id;
         delete ret.password;
-        ret.Rcoins = doc.Rcoins;
+        ret.Rcoins = Number(doc.Rcoins).toFixed(2);
         ret.VIPLevel = vipCoculart(doc.growthPoints);
         if (doc.created_at && doc.updated_at) {
             ret.created_at = new Date(doc.created_at).getTime();
